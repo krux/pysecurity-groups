3f8234cb1b30f25785e025c275e1cb9f04df7a8c
Paul Lathrop <paul@tertiusfamily.net>
2012-05-21 11:14:13 -0700

    Rename to pysecurity_groups

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@
 
 from setuptools import setup, find_packages
 
-setup(name='ksecurity_groups',
+setup(name='pysecurity_groups',
       version="0.0.1",
       description='Library for keeping EC2 security groups synchronized.',
       author='Paul Lathrop',
       author_email='paul@krux.com',
-      url='https://github.com/krux/ksecurity_groups',
+      url='https://github.com/krux/pysecurity_groups',
       packages=find_packages(),
       install_requires=['boto'],
       tests_require=['nose', 'coverage'],
