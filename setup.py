096f6b168df883bd78a03b5ae9f9296d97e3d389
Paul Lathrop <paul@tertiusfamily.net>
2012-05-22 13:11:28 -0700

    pysecurity_groups -> pysecurity-groups where it doesn't matter for the computer.

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@
 
 from setuptools import setup, find_packages
 
-setup(name='pysecurity_groups',
+setup(name='pysecurity-groups',
       version="0.0.1",
       description='Library for working with EC2 security groups in bulk.',
       author='Paul Lathrop',
       author_email='paul@krux.com',
-      url='https://github.com/krux/pysecurity_groups',
+      url='https://github.com/krux/pysecurity-groups',
       packages=find_packages(),
       install_requires=['boto'],
       tests_require=['nose', 'coverage'],
